ef54aa9e00f20a72e93c2bc9fe9e26bd5a9c1094
hsuyh <hsuyh82716@gmail.com>
2019-08-14 01:08:53 +0800

    Measure execution time in user space

--- client.c
+++ client.c
@@ -3,39 +3,60 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
 #define FIB_DEV "/dev/fibonacci"
 
 int main()
 {
+    int fd, fd_out;
     long long sz;
+
+    clockid_t cid = 1;
+    struct timespec start, end;
 
     char buf[1];
     char write_buf[] = "testing writing";
-    int offset = 100; /* TODO: try test something bigger than the limit */
+    int offset = 100;  // TODO: test something bigger than the limit
+    int i = 0;
 
-    int fd = open(FIB_DEV, O_RDWR);
+    fd = open(FIB_DEV, O_RDWR);
+    fd_out = open("client_output.txt", O_RDWR | O_CREAT | O_TRUNC, 644);
+
     if (fd < 0) {
         perror("Failed to open character device");
         exit(1);
     }
 
-    for (int i = 0; i <= offset; i++) {
+    for (i = 0; i <= offset; i++) {
         sz = write(fd, write_buf, strlen(write_buf));
         printf("Writing to " FIB_DEV ", returned the sequence %lld\n", sz);
     }
 
-    for (int i = 0; i <= offset; i++) {
+    char buffer[300];
+    char kbuffer[300];
+
+    for (i = 0; i <= offset; i++) {
         lseek(fd, i, SEEK_SET);
-        sz = read(fd, buf, 1);
+
+        clock_gettime(cid, &start);
+        sz = read(fd, kbuffer, 1);
+
+        clock_gettime(cid, &end);
+        long int sec, nanosec;
+        sec = end.tv_sec - start.tv_sec;
+        nanosec = end.tv_nsec - start.tv_nsec;
+        snprintf(buffer, 300, "%d %ld %ld\n", i, sec * 1000000000 + nanosec,
+                 atol(kbuffer));
+        write(fd_out, buffer, strlen(buffer));
         printf("Reading from " FIB_DEV
                " at offset %d, returned the sequence "
                "%lld.\n",
                i, sz);
     }
 
-    for (int i = offset; i >= 0; i--) {
+    for (i = offset; i >= 0; i--) {
         lseek(fd, i, SEEK_SET);
         sz = read(fd, buf, 1);
         printf("Reading from " FIB_DEV
@@ -45,5 +66,6 @@ int main()
     }
 
     close(fd);
+    close(fd_out);
     return 0;
 }
